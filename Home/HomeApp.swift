a49feb79986be01dafc1082b668918bf8af76d83
Yoshi Miyakawa <ymiyakawa@lcs.on.ca>
2024-01-11 14:06:03 -0500

    Initial Commit

---
+++ Home/HomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomeApp.swift
+//  Home
+//
+//  Created by 宮川義之助 on 2024/01/11.
+//
+
+import SwiftUI
+
+@main
+struct HomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
